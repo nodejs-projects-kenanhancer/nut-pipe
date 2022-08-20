5f0c68f19c045ff327c6c8f418443eedd6d10087
kenanhancer <kenan.hancer@howdengroup.com>
2022-08-20 11:28:10 +0200

    bugfix: github actions7

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,13 @@
 module.exports = {
     testEnvironment: 'node',
+    rootDir: ".",
+    // modulePaths: ['<rootDir>'],
     transform: { "^.+\\.(t|j)s$": "ts-jest" },
     testMatch: ['<rootDir>/tests/**/*.test.(t|j)s'],
     collectCoverageFrom: ['<rootDir>/src/index.js'],
-    modulePaths: ['<rootDir>'],
     moduleFileExtensions: ["js", "json", "ts"],
     coverageDirectory: './coverage',
+    coverageReporters: ["json", "lcov", "text", "cobertura"],
     testPathIgnorePatterns: ['/node_modules/', '/dist/', '/coverage/', '/.github/'],
     verbose: true,
     globals: {
